b920dc1ce840ccb6d8de8bf52352224e4ecb30e4
JustinKosimov <jasurkosimovmj@gmail.com>
2024-01-29 13:56:02 +0500

    pull header

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,48 @@
+import "./Hero.scss"
+// import imgs
+import HeroImg1 from "../../assets/img/hero-slider.png"
+// import swipe
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+
+function Hero() {
+    return(
+        <>
+<div className="container">
+      
+         <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={HeroImg1} alt="" /> </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      </div>
+        </>
+    )
+}
+
+export default Hero
